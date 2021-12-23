4dd0cc2fe253de8ff25199f3fcec1c8e314f6be9
abdizamed <mahirscode@gmail.com>
2021-12-23 14:40:06 +0300

    JavaScript Analogue Project

---
+++ js/App.js
@@ -0,0 +1,18 @@
+const hourHand = document.querySelector('#hour-hand');
+const minuteHand = document.querySelector('#minute-hand');
+const secondHand = document.querySelector('#second-hand');
+
+
+
+
+const myDate = () => {
+    const theDate = new Date();
+    const seconds = (theDate.getSeconds) / 60;
+    const minutes = (seconds + theDate.getSeconds) / 60;
+    const hours = (minutes + theDate.getSeconds) / 12;
+
+}
+
+
+setInterval(myDate, 1000)
+
