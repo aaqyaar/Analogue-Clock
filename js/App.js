4b61f09bcf2a1f06127d1721d32a434c4873128f
abdizamed <mahirscode@gmail.com>
2021-12-23 14:42:51 +0300

    JavaScript Analogue Clock

--- js/App.js
+++ js/App.js
@@ -2,15 +2,11 @@ const hourHand = document.querySelector('#hour-hand');
 const minuteHand = document.querySelector('#minute-hand');
 const secondHand = document.querySelector('#second-hand');
 
-
-
-
 const myDate = () => {
     const theDate = new Date();
     const seconds = (theDate.getSeconds) / 60;
     const minutes = (seconds + theDate.getSeconds) / 60;
     const hours = (minutes + theDate.getSeconds) / 12;
-
 }
 
 
