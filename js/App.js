ac6dedf78c0b4ac3d4f2f0e93da61864362b584b
abdizamed <abdizamedmo@gmail.com>
2021-12-27 19:16:41 +0300

    JavaScript Analogue Clock v4

--- js/App.js
+++ js/App.js
@@ -1,13 +1,24 @@
-const hourHand = document.querySelector('#hour-hand');
-const minuteHand = document.querySelector('#minute-hand');
-const secondHand = document.querySelector('#second-hand');
+setInterval(() => {
+    setClock()
+}, 1000)
 
-const myDate = () => {
-    const theDate = new Date();
-    const seconds = (theDate.getSeconds) / 60;
-    const minutes = (seconds + theDate.getSeconds) / 60;
-    const hours = (minutes + theDate.getSeconds) / 12;
+const hourHand = document.querySelector('#hour')
+const minuteHand = document.querySelector('#minute')
+const secondHand = document.querySelector('#second')
+
+const setClock = () => {
+    const theDate = new Date()
+    const seconds = theDate.getSeconds() / 60;
+    const minutes = (seconds + theDate.getMinutes()) / 60;
+    const hours = (minutes + theDate.getHours()) / 12;
+
+    setRotation(secondHand, seconds)
+    setRotation(minuteHand, minutes)
+    setRotation(hourHand, hours)
 }
 
-setInterval(myDate, 1000)
+function setRotation(element, rotation) {
+    element.style.setProperty('--rotation', rotation * 360)
+}
 
+setClock()
